2ba6e6bb32916b34b81d4e9786975b6bd4b420fb
Petr Pivoňka <petrpiv1@gmail.com>
2026-07-21 01:56:25 +0200

    Fix MP4 vehicle order

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ var referenceDir = Path.Combine(projectDir, "Reference");
 var gameVehicleOrder = new Dictionary<string, (string DisplayName, string FileBaseName)[]>
 {
     ["TM2020"] = [("StadiumCar", "TuningsSport"), ("SnowCar", "TuningsSnow"), ("RallyCar", "CarRally"), ("DesertCar", "DesertCar")],
-    ["MP4"] = [("CanyonCar", "CanyonCar"), ("StadiumCar", "StadiumCar"), ("ValleyCar", "ValleyCar"), ("LagoonCar", "LagoonCar"), ("TrafficCar", "TrafficCar"), ("DesertCar", "American"), ("SnowCar", "Buggy"), ("RallyCar", "Rally"), ("IslandCar", "Sport"), ("BayCar", "BayCar"), ("CoastCar", "CoastCar")],
+    ["MP4"] = [("CanyonCar", "CanyonCar"), ("StadiumCar", "StadiumCar"), ("ValleyCar", "ValleyCar"), ("LagoonCar", "LagoonCar"), ("TrafficCar", "TrafficCar"), ("DesertCar", "DesertCar"), ("SnowCar", "Buggy"), ("RallyCar", "RallyCar"), ("IslandCar", "IslandCar"), ("BayCar", "BayCar"), ("CoastCar", "CoastCar")],
     ["TMT"] = [("CanyonCar", "CanyonCar"), ("StadiumCar", "StadiumCar"), ("ValleyCar", "ValleyCar"), ("LagoonCar", "LagoonCar")],
     ["TMF"] = [("DesertCar", "American"), ("SnowCar", "Buggy"), ("RallyCar", "Rally"), ("IslandCar", "Sport"), ("BayCar", "BayCar"), ("CoastCar", "CoastCar"), ("StadiumCar", "StadiumCar")],
     ["TMU"] = [("DesertCar", "American"), ("SnowCar", "Buggy"), ("RallyCar", "Rally"), ("IslandCar", "Sport"), ("BayCar", "BayCar"), ("CoastCar", "CoastCar"), ("StadiumCar", "StadiumCar")],
